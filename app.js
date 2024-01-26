5da7744d1d75a8b41cf40962d2a3e9c55c810edd
Plouffe <e2395295@cmaisonneuve.qc.ca>
2024-01-26 14:51:55 -0500

    second commit

--- app.js
+++ app.js
@@ -1,26 +1,70 @@
-const http = require("http");
+const express = require("express");
 const dotenv = require("dotenv");
 const fs = require("fs");
+const path = require("path");
+const mustacheExpress = require("mustache-express");
 
-//Au début du fichier
+//Configuration
 dotenv.config();
 
-//Même chose qu'écrire function
-const server = http.createServer((request,response)=>{
-    if(request.method == "GET" && request.url == "/"){
-        const file = fs.readFileSync("./public/index.html", "utf-8");
-        response.setHeader("Content-Type", "text/html");
-        response.statusCode = 200;
+const server = express();
+////////////////////////////
 
-        response.end(file);
-    } else {
-        const file = fs.readFileSync("./public/404.html", "utf-8");
-        response.setHeader("Content-Type", "text/html");
-        response.statusCode = 404;
-        
-        response.end(file);
-    }
+server.set("views", path.join(__dirname, "views"));
+server.set("view engine", "mustache");
+server.engine("mustache", mustacheExpress());
+
+
+//Middlewares
+//Doit être avant les points d'accès
+
+//Done tout les fichiers du dossier public. Dans le font, ça fait une shit ton de demandes get au serveur pour chaque fichier.
+server.use(express.static(path.join(__dirname, "public")));
+
+
+//Points d'accès    (Arrête la requête)
+server.get("/donnees",(req, res)=>{
+    //Ceci sera remplacé par un fetch ou un appel à la base de données
+    const donnees = require("./data/donneesTest.js");
+    res.statusCode = 200;
+    res.json(donnees);
 });
+
+
+/**
+ * @method get
+ * @param id
+ * Permet d'accéder à un utilisateur
+ */
+server.get("/donnees/:id", (req, res)=>{
+    console.log(req.params.id);
+    const donnees = require("./data/donneesTest.js");
+
+    const utilisateur = donnees.find((element)=>{
+        //Va check si il y en a
+        return element.id == req.params.id;
+    });
+
+    if (utilisateur) {
+        res.statusCode = 200;
+        res.json(utilisateur);
+    } else {
+        res.statusCode = 404;
+        res.json({message: "Utilisateur non trouvé"});
+    }
+    res.send(req.params.id);
+});
+
+
+//DOIT être en dernier
+//Gestion page 404 - requête non trouvée
+
+server.use((req, res)=>{
+    res.statusCode = 404;
+    res.render("404", {url: req.url});
+})
+
+
 
 server.listen(process.env.PORT, ()=>{
     console.log("Le serveur est démarré");
